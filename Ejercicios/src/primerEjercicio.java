476583a28354a246f0ad5884e885cddd0b72016a
Ilan Néstor Angeles Rodriguez <ilanangelesrodriguez@gmail.com>
2022-09-20 14:31:19 -0500

    Agrego el primer ejercicio

---
+++ Ejercicios/src/primerEjercicio.java
@@ -0,0 +1,22 @@
+/*Primera parte:
+Crear una función con tres parámetros que sean números que se suman entre sí.
+Llamar a la función en el main y darle valores.
+*/
+import java.util.Scanner;
+public class primerEjercicio {
+    public static void main(String[] args) {
+        System.out.println("---Calculadora---");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Introduce el primer valor: ");
+        int a=sc.nextInt();
+        System.out.print("Introduce el segundo valor: ");
+        int b=sc.nextInt();
+        System.out.print("Introduce el tercer valor: ");
+        int c=sc.nextInt();
+        System.out.print("La suma de los valores es " + suma(a,b,c));
+        //suma(a,b,c);
+    }
+    public static int suma(int a, int b, int c){
+        return a+b+c;
+    }
+}
