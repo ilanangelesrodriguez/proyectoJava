08db962a5b10be37119560f8b69fb366ec7f8526
Ilan Néstor Angeles Rodriguez <ilanangelesrodriguez@gmail.com>
2022-09-22 01:44:07 -0500

    Agrego bucles while y do while

---
+++ sentenciasDeControl/src/buclesWhileYDoWhile.java
@@ -0,0 +1,9 @@
+public class buclesWhileYDoWhile {
+    public static void main(String[] args) {
+        int contador=10;
+        while (contador>0){
+            System.out.println("El contador es: "+contador);
+            contador=contador-1;
+        }
+    }
+}
