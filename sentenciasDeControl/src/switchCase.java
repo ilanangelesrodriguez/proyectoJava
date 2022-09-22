839b673a3a75d3df6288c082e39e283962338fd4
Ilan Néstor Angeles Rodriguez <ilanangelesrodriguez@gmail.com>
2022-09-22 01:45:09 -0500

    Agrego sentencia Switch

---
+++ sentenciasDeControl/src/switchCase.java
@@ -0,0 +1,21 @@
+public class switchCase {
+    public static void main(String[] args) {
+        var club="FC Barcelona";
+        switch(club){
+            case "FC Barcelona":
+            System.out.println("Es de LaLiga");
+            break;
+            case "AC Milan":
+            System.out.println("Es de la Serie A");
+            break;
+            case "Borussia Dortmund":
+            System.out.println("Es de la Bundesliga");
+            break;
+            case "Chelsea FC":
+            System.out.println("Es de la Premier league");
+            break;
+            default:
+            System.out.println("Pertenece a otra liga");
+        }
+    }
+}
