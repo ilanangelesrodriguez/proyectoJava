1c98f9bbe58294d94778d6a6a452304e3924ab88
Ilan Néstor Angeles Rodriguez <ilanangelesrodriguez@gmail.com>
2022-09-22 01:43:02 -0500

    Agrego bucle for

---
+++ sentenciasDeControl/src/buclesFor.java
@@ -0,0 +1,9 @@
+public class buclesFor {
+    public static void main(String[] args) {
+        //int valores[]=new int[5];
+        int valores[]={10,20,30,40,50};
+        for(int i=0;i<valores.length;i++){
+            System.out.println(valores[i]);
+        }
+    }
+}
