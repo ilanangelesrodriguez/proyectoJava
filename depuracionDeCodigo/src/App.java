bc61b4013f89369f0fa24118416c92c96e91b323
Ilan Néstor Angeles Rodriguez <ilanangelesrodriguez@gmail.com>
2022-09-22 18:50:01 -0500

    Agrego ejemplo de depuracion de codigo

---
+++ depuracionDeCodigo/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hola, mundo!");
+        int suma=0;
+        System.out.println(".");
+        suma=suma+15;
+
+        System.out.println(suma);
+    }
+}
