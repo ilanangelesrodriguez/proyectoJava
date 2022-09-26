714d42be1507dbf4e62942228d87a7a69902495b
Ilan Néstor Angeles Rodriguez <ilanangelesrodriguez@gmail.com>
2022-09-26 03:13:22 -0500

    Agrego el ejercicio 4

--- Ejercicios/src/cuartoEjercicio.java
+++ Ejercicios/src/cuartoEjercicio.java
@@ -6,10 +6,15 @@ public class cuartoEjercicio {
         c1.edad=20;
         c1.telefono=123456789;
         c1.credito = 1500;
-        System.out.println("Cliente: "+c1.nombre);
-        System.out.println("Edad: "+c1.edad+" años");
-        System.out.println("Telefono: "+c1.telefono);
-        System.out.println("Credito: S/."+c1.credito);
+
+        System.out.println("|---------------------|");
+        System.out.println("|*+---- Cliente ----+*|");
+        System.out.println("|---------------------|");
+        System.out.println("| Cliente: "+c1.nombre);
+        System.out.println("| Edad: "+c1.edad+" años");
+        System.out.println("| Telefono: "+c1.telefono);
+        System.out.println("| Credito: S/."+c1.credito);
+        System.out.println("|---------------------|");
         
         trabajador t1=new trabajador();
         t1.nombre="EZ Abde";
@@ -17,20 +22,25 @@ public class cuartoEjercicio {
         t1.telefono=243576819;
         t1.salario = 1300;
 
-        System.out.println("Trabajador: "+t1.nombre);
-        System.out.println("Edad: "+t1.edad+" años");
-        System.out.println("Telefono: "+t1.telefono);
-        System.out.println("Salario: S/."+t1.credito);
+        System.out.println("|---------------------|");
+        System.out.println("|*+--- Trabajador --+*|");
+        System.out.println("|---------------------|");
+        System.out.println("| Trabajador: "+t1.nombre);
+        System.out.println("| Edad: "+t1.edad+" años");
+        System.out.println("| Telefono: "+t1.telefono);
+        System.out.println("| Salario: S/."+t1.salario);
+        System.out.println("|---------------------|");
     }
+
 }
-class persona{
-    int edad=20;
+class Person{
+    int edad;
     int telefono;
     String nombre;
 }
-class cliente extends persona{
+class cliente extends Person{
     int credito;
 }
-class trabajador extends persona {
+class trabajador extends Person{
     int salario;
 }
