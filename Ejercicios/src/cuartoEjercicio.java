edafd0ad133283202399743bb434e5ba450fc019
Ilan Néstor Angeles Rodriguez <ilanangelesrodriguez@gmail.com>
2022-09-26 02:25:20 -0500

    Modificación del cuarto ejercicio
    Ordeno el metodo main.

--- Ejercicios/src/cuartoEjercicio.java
+++ Ejercicios/src/cuartoEjercicio.java
@@ -1,24 +1,35 @@
 public class cuartoEjercicio {
     public static void main(String[] args) {
         cliente c1=new cliente();
-        c1.propiedades();
-
+        
+        c1.nombre="Ilan";
+        c1.edad=20;
+        c1.telefono=123456789;
+        c1.credito = 1500;
+        System.out.println("Cliente: "+c1.nombre);
+        System.out.println("Edad: "+c1.edad+" años");
+        System.out.println("Telefono: "+c1.telefono);
+        System.out.println("Credito: S/."+c1.credito);
+        
         trabajador t1=new trabajador();
-        int s=t1.salario=1500;
+        t1.nombre="EZ Abde";
+        t1.edad=21;
+        t1.telefono=243576819;
+        t1.salario = 1300;
 
-        System.out.println("El salario es: "+s);
+        System.out.println("Trabajador: "+t1.nombre);
+        System.out.println("Edad: "+t1.edad+" años");
+        System.out.println("Telefono: "+t1.telefono);
+        System.out.println("Salario: S/."+t1.credito);
     }
-
 }
 class persona{
     int edad=20;
-    int telefono=123456789;
-    String nombre="Ilan";
+    int telefono;
+    String nombre;
 }
 class cliente extends persona{
-    public void propiedades(){
-        int credito=1000;
-    }
+    int credito;
 }
 class trabajador extends persona {
     int salario;
